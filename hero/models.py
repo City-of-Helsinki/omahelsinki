7975fa763fd04dddc7f764f8416cc139f30d8099
olujuwon <alabyseyi@yahoo.com>
2018-07-04 14:40:26 +0300

    model migration

--- hero/models.py
+++ hero/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.search import index
+
+
+class Hero(Page):
+    heroHeading = models.CharField(max_length=100, blank=True)
+    heroBody = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('heroHeading'),
+        index.SearchField('heroBody'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('heroHeading'),
+        FieldPanel('heroBody', classname="full"),
+    ]
