9cb5d2080f80f9bbbc085963a4ffe04aae08cfe8
olujuwon <alabyseyi@yahoo.com>
2018-07-18 11:51:34 +0300

    services section added

--- services/migrations/0001_initial.py
+++ services/migrations/0001_initial.py
@@ -2,7 +2,6 @@
 
 from django.db import migrations, models
 import django.db.models.deletion
-import wagtail.core.fields
 
 
 class Migration(migrations.Migration):
@@ -11,19 +10,18 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('wagtailcore', '0040_page_draft_title'),
+        ('wagtailimages', '0020_add-verbose-name'),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='HomePage',
+            name='Services',
             fields=[
                 ('page_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='wagtailcore.Page')),
-                ('heading', models.CharField(blank=True, max_length=100)),
-                ('body', wagtail.core.fields.RichTextField(blank=True)),
-                ('login_link', models.CharField(blank=True, max_length=500)),
-                ('login_text', models.CharField(blank=True, max_length=500)),
-                ('register_link', models.CharField(blank=True, max_length=500)),
-                ('register_text', models.CharField(blank=True, max_length=500)),
+                ('caption_head', models.CharField(blank=True, max_length=100)),
+                ('caption', models.CharField(blank=True, max_length=250)),
+                ('service_link', models.CharField(blank=True, max_length=250)),
+                ('image', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='+', to='wagtailimages.Image')),
             ],
             options={
                 'abstract': False,
