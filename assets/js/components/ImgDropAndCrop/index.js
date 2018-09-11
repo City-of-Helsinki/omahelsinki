787cf8889888bbb41013a7bd0251611c403374b1
Rasbin Rijal <rasbin.rijal@gmail.com>
2018-09-11 17:48:14 +0300

    Initial structure created for image drop or upload using react-dropzone

--- assets/js/components/ImgDropAndCrop/index.js
+++ assets/js/components/ImgDropAndCrop/index.js
@@ -1,12 +1,74 @@
 import React, {Component} from 'react'
 import Dropzone from 'react-dropzone'
 
+const imageMaxSize = 100000000
+const acceptedFileTypes = 'image/x-png, image/png, image/jpg, image/jpeg, image/gif'
+const acceptedFileTypesArray = acceptedFileTypes.split(',').map((item) => {return item.trim()})
 class ImgDropAndCrop extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            imgSrc: null,
+        }
+    }
+
+    verifyFile = (files) => {
+        if (files && files.length > 0) {
+            const currentFile = files[0]
+            const currentFileType = currentFile.type
+            const currentFileSize = currentFile.size
+            if(currentFileSize > imageMaxSize) {
+                alert('This file is not allowed.' + currentFileSize + 'bytes is too large')
+                return false
+            }
+            if (!acceptedFileTypesArray.includes(currentFileType)){
+                alert('This file is not allowed. Only images are allowed.')
+                return false
+            }
+            return true
+        }
+    }
+
+    handleOnDrop = (files, rejectedFiles) => {
+        if (rejectedFiles && rejectedFiles.length > 0) {
+            this.verifyFile(rejectedFiles)
+        }
+
+        if(files && files.length > 0) {
+            const isVerified = this.verifyFile(files)
+            if (isVerified) {
+                const currentFile = files[0]
+                const myFileItemReader = new FileReader()
+                myFileItemReader.addEventListener('load', () => {
+                    console.log(myFileItemReader.result)
+                    this.setState({
+                        imgSrc: myFileItemReader.result,
+                    })
+                }, false)
+
+                myFileItemReader.readAsDataURL(currentFile)
+            }
+        }
+    }
+
+
     render() {
+        const {imgSrc} = this.state
         return(
             <div>
                 <h1>Drag and Crop</h1>
-                <Dropzone>Drop File Here</Dropzone>
+                {imgSrc !== null ?
+                    <div>
+                        {imgSrc}
+                        <img src={imgSrc} alt='some text here' /> 
+                    </div> : 
+                    <Dropzone
+                        onDrop={this.handleOnDrop}
+                        multiple={false}
+                        maxSize={imageMaxSize}
+                        accept={acceptedFileTypes} 
+                    >Drop image here / Click to upload</Dropzone>
+                }
             </div>
         )
     }
