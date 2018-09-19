7c0d812f979a6811bb13faf678a0543f43ab6c3c
Jimi <jiripim@gmail.com>
2018-09-19 13:36:03 +0300

    Very simple Error component

---
+++ assets/js/components/Error/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {FormattedMessage} from 'react-intl'
+
+const Error = props => {
+    return (
+        <div>
+            <FormattedMessage id='app.error'/>
+        </div>
+    )
+}
+
+export default Error
