f394cd0ce683df964b723dfdb05b60a30fcb536b
Rasbin Rijal <rasbin.rijal@gmail.com>
2018-09-23 22:37:47 +0300

    Canvas of cropped image removed

--- assets/js/components/ImgDropAndCrop/index.js
+++ assets/js/components/ImgDropAndCrop/index.js
@@ -103,7 +103,7 @@ class ImgDropAndCrop extends Component {
                             onChange={this.handleOnCropChange}/>
                         <br />
                         <p>Preview Canvas Crop</p>
-                        <canvas ref={this.imagePreviewCanvasRef} style={{maxWidth: '135px'}}></canvas>
+                        <canvas ref={this.imagePreviewCanvasRef} style={{maxWidth: '135px', display: 'none'}}></canvas>
                     </div> 
                     : 
                     <Dropzone
