88e113eee2be8146ed443a84e96d46a09ecb36d3
Rasbin Rijal <rasbin.rijal@gmail.com>
2018-10-01 15:41:13 +0300

    Initial api call structure

--- assets/js/components/DownloadOwnData/index.js
+++ assets/js/components/DownloadOwnData/index.js
@@ -1,13 +1,41 @@
 import React, {Component} from 'react'
 import {Button} from 'reactstrap'
+import axios from 'axios'
 
 class DownloadOwnData extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            profileData: [],
+        }
+    }
+
+    componentDidMount() {
+        const token = window.TUNNISTAMO_ACCESS_TOKEN
+        const config = {
+            headers: {'Authorization': 'Bearer' + token},
+        }
+
+        axios.get('https://profile-api.test.hel.ninja/profile-test/v1/profile/', config)
+            .then(res => {
+                console.log(res)
+                this.setState({
+                    profileData: res.data.results,
+                })
+            })
+    }
+
+    handleDownloadData = (event) => {
+        event.preventDefault()
+    }
+
     render() {
         return(
             <div>
                 <Button
                     color="primary"
                     className="downloadButton"
+                    onClick={this.handleDownloadData}
                 >
                 Download Profile Data
                 </Button>
