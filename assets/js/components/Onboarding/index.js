046a95dae06a0583984bdd8a47d7a581f488695d
Jimi <jiripim@gmail.com>
2018-10-08 16:48:22 +0300

    Changed the way interests in onboarding is handled

--- assets/js/components/Onboarding/index.js
+++ assets/js/components/Onboarding/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import {Wizard, Steps, Step} from 'react-albus';
+import React from 'react'
+import {Wizard, Steps, Step} from 'react-albus'
 import {connect} from 'react-redux'
+import {Container} from 'reactstrap'
+
 import {updateUserData} from '../../user/redux'
 import {
     StepButtons, 
@@ -9,50 +11,31 @@ import {
     //CreatePassword, 
     //Settings,
     Interest,
-} from '../OnboardingSteps';
+} from '../OnboardingSteps'
 
-import {Container} from 'reactstrap'
 import HelIcon from '../HelIcon'
 
 class Onboarding extends React.Component {
 
     constructor(props) {
-        super(props);
+        super(props)
 
         this.state = {
-            // firstname: '',
-            // lastname: '',
-            // email: '',
-            // ofAge: false,
-            // password: '',
-            // passwordRepeat: '',
-            // enableNotifications: '',
-            // enabledMessages: [],
-            // enabledNotifications: [],
-            selectedFields: [],
-            selectedOption: '',
-        };
-
-        //this.handleChange = this.handleChange.bind(this);
-        this.wizardFinished = this.wizardFinished.bind(this);
-    }
-    onSelect = (selected) => {
-        const {selectedFields} = this.state
-
-        const index = selectedFields.indexOf(selected);
-        if (index < 0) {
-            selectedFields.push(selected);
-        } else {
-            selectedFields.splice(index, 1);
+            interests: [],
+            regions: [],
         }
-        this.setState({selectedFields: [...selectedFields]});
+
+        this.wizardFinished = this.wizardFinished.bind(this)
+        this.interestsChanged = this.interestsChanged.bind(this)
+        this.regionsChanged = this.regionsChanged.bind(this)
     }
 
-    // handleChange(data) {
-    //     this.setState(data);
-    // }
-    handleChange = (selectedOption) => {
-        this.setState({selectedOption});
+    interestsChanged(interests) {
+        this.setState({interests})
+    }
+
+    regionsChanged(regions) {
+        this.setState({regions})
     }
 
     wizardFinished() {
@@ -61,16 +44,10 @@ class Onboarding extends React.Component {
             region: this.state.selectedOption,
         }
         this.props.updateUserData(userInterest)
-        
     }
 
     render() {
-        // const {firstname, lastname, email, ofAge} = this.state;
-        //const personalInformation = {firstname, lastname, email, ofAge};
-        //const {password, passwordRepeat} = this.state;
-        //const passwordData = {password, passwordRepeat};
-        //const {enabledNotifications, enabledMessages, enableNotifications} = this.state;
-        //const settings = {enabledNotifications, enabledMessages, enableNotifications};
+        const {interests, regions} = this.state
         return (
             <div className="oma-onboarding-wrapper">
                 <Container>
@@ -85,43 +62,24 @@ class Onboarding extends React.Component {
                                         <Step id='welcome'>
                                             <Welcome />
                                         </Step>
-                                        {/* <Step id='personalInformation'>
-                                            <PersonalInformation
-                                                data={personalInformation}
-                                                onChange={this.handleChange}
-                                            />
-                                        </Step> */}
-                                        {/* <Step id='createPassword'>
-                                            <CreatePassword
-                                                data={passwordData}
-                                                onChange={this.handleChange}
-                                            />
-                                        </Step> */}
                                         <Step id='interests'>
-                                            <Interest 
-                                                onSelect={this.onSelect}
-                                                selectedFields={this.state.selectedFields}
-                                                selectedOption={this.state.selectedOption}
-                                                handleChange={this.handleChange}
+                                            <Interest
+                                                selectedInterests={interests}
+                                                onInterestsChanged={this.interestsChanged}
+                                                selectedRegions={regions}
+                                                onRegionsChanged={this.regionsChanged}
                                             />
                                         </Step>
-                                        {/* <Step id='settings'>
-                                            <Settings
-                                                data={settings}
-                                                onChange={this.handleChange}
-                                            />
-                                        </Step> */}
                                     </Steps>
                                 </div>
-                                
                                 <StepButtons onFinish={this.wizardFinished} />
                             </Wizard>
                         </div>
                     </div>
                 </Container>
             </div>
-        );
+        )
     }
 }
 
-export default connect(null, {updateUserData})(Onboarding);
+export default connect(null, {updateUserData})(Onboarding)
