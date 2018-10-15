e3ce5b850129d8fd419585ebe53630e2c64b3171
Sanjog <sanjogstha7@gmail.com>
2018-10-15 11:07:28 +0300

    change dev localhost

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,43 +4,43 @@ const common = require('./webpack.common.js');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = merge(common, {
-  mode: 'development',
-  entry: [
-    'webpack-dev-server/client?http://0.0.0.0:3000',
-    'webpack/hot/only-dev-server',
-  ],
-  output: {
-      publicPath: 'http://0.0.0.0:3000/assets/bundles/',
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(), // don't reload if there is an error
-    // new BundleAnalyzerPlugin(),
-  ],
-  module: {
-    rules: [
-      // we pass the output from babel loader to react-hot loader
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            cacheDirectory: true,
-            plugins: ['react-hot-loader/babel'],
-          }
-        }],
-      }
+    mode: 'development',
+    entry: [
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
     ],
-  },
+    output: {
+        publicPath: 'http://localhost:3000/assets/bundles/',
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(), // don't reload if there is an error
+    // new BundleAnalyzerPlugin(),
+    ],
+    module: {
+        rules: [
+            // we pass the output from babel loader to react-hot loader
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true,
+                        plugins: ['react-hot-loader/babel'],
+                    },
+                }],
+            },
+        ],
+    },
 
-  devServer: {
-    port: 3000,
-    host: "0.0.0.0",
-    hot: true,
-    inline: true,
-    headers: {
-      "Access-Control-Allow-Origin": "*"
-    }
-  }
+    devServer: {
+        port: 3000,
+        //host: "localhost",
+        hot: true,
+        inline: true,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        },
+    },
 });
