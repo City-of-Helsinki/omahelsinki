fbd1e59b88952482ec251cd2b2854a3edbfb653b
Janimatti Ellonen <janimatti.ellonen@fraktio.fi>
2018-12-04 10:19:14 +0200

    using !isEmpty instead of !== 0

--- assets/js/components/Tab/Service/Service.js
+++ assets/js/components/Tab/Service/Service.js
@@ -3,6 +3,7 @@ import { Row, Col } from 'reactstrap'
 import { FormattedMessage } from 'react-intl'
 import { connect } from 'react-redux'
 import find from 'lodash/find'
+import isEmpty from 'lodash/isEmpty'
 
 import Loading from '../../Loading'
 import HelCollapsibleField from '../../HelCollapsibleField'
@@ -73,7 +74,7 @@ class ServiceTab extends Component {
             </Col>
           </Row>
         </section>
-        {unusedServices.length !== 0 && (
+        {!isEmpty(unusedServices) && (
           <section>
             <h2>
               <FormattedMessage id="app.services.unconnectedHeading" />
